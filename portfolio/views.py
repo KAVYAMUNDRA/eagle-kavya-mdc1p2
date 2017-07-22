f095382924fee5bcd488bddc7136f4c0832b35dd
kmundra123 <mundra.kavay@gmail.com>
2017-07-22 11:46:42 -0500

    message

--- portfolio/views.py
+++ portfolio/views.py
@@ -110,7 +110,7 @@ def investment_edit(request, pk):
            # investment.customer = investment.id
            investment.updated_date = timezone.now()
            investment.save()
-           investments = Investment.objects.filter(purchase_date__lte=timezone.now())
+           investments = Investment.objects.filter(acquired_date__lte=timezone.now())
            return render(request, 'portfolio/investment_list.html', {'investments': investments})
    else:
        # print("else")
@@ -125,7 +125,7 @@ def investment_new(request):
            investment = form.save(commit=False)
            investment.created_date = timezone.now()
            investment.save()
-           investments = Investment.objects.filter(purchase_date__lte=timezone.now())
+           investments = Investment.objects.filter(acquired_date__lte=timezone.now())
            return render(request, 'portfolio/investment_list.html',
                          {'investments': investments})
    else:
@@ -137,7 +137,7 @@ def investment_new(request):
 def investment_delete(request, pk):
    investment = get_object_or_404(Investment, pk=pk)
    investment.delete()
-   investments = Investment.objects.filter(purchase_date__lte=timezone.now())
+   investments = Investment.objects.filter(acquired_date__lte=timezone.now())
    return render(request, 'portfolio/investment_list.html', {'investments': investments})
 
 @login_required
